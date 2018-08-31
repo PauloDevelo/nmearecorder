9d70c55a1b992536bcef78d5fd965793c9b057fd
Torruella, Paul <paul.torruella@suez.com>
2018-08-31 15:14:02 +1100

    Improve the terminaison of the service.

--- nmearecorder/src/main/java/arbutus/ArbutusBlackBox.java
+++ nmearecorder/src/main/java/arbutus/ArbutusBlackBox.java
@@ -9,7 +9,6 @@ import arbutus.nmea.service.NMEAService;
 import arbutus.rtmodel.Arbutus;
 import arbutus.service.ServiceManager;
 import arbutus.timeservice.ITimeService;
-import arbutus.timeservice.SynchronizationException;
 import arbutus.timeservice.TimeService;
 
 
@@ -18,56 +17,51 @@ public class ArbutusBlackBox {
 	
 	private Arbutus arbutus = new Arbutus();
 	
-	private static boolean turnOff = false;
-	
-	public synchronized boolean isTurnedOff() {
-		return turnOff;
-	}
-	
-	public static synchronized void turnOff() {
-		turnOff = true;
-	}
-	
 	public static void main(String[] args) {
-		ServiceManager srvMgr = ServiceManager.getInstance();
-		
-		srvMgr.register(INMEAService.class, new NMEAService());
-		srvMgr.register(ITimeService.class, new TimeService());
-		srvMgr.register(IInfluxdbRepository.class, new InfluxdbRepository());
-		
-		srvMgr.startServices();
-		
 		Runtime.getRuntime().addShutdownHook(new Thread()
         {
+			private Thread currentThread = Thread.currentThread();
+			
             @Override
             public void run()
             {
                 log.debug("Shutdown NMEARecorder");
-                ArbutusBlackBox.turnOff();
-                
-                
+                currentThread.interrupt();
             }
         });
 		
 		ArbutusBlackBox blackBox = new ArbutusBlackBox();
-		blackBox.waitMethod();
+		blackBox.run();
 	}
 	
-	private synchronized void waitMethod() {
-		ITimeService timeService = ServiceManager.getInstance().getService(ITimeService.class);
-		while (!isTurnedOff()) {
-			try {
-				this.wait(2000);
-			} catch (InterruptedException e) {
-				log.error(e);
+	private synchronized void run() {
+		try {
+			ServiceManager srvMgr = ServiceManager.getInstance();
+			
+			srvMgr.register(INMEAService.class, new NMEAService());
+			srvMgr.register(ITimeService.class, new TimeService());
+			srvMgr.register(IInfluxdbRepository.class, new InfluxdbRepository());
+			
+			srvMgr.startServices();
+			
+			while (!Thread.interrupted()) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					log.debug("Interruption received");
+				}
 			}
 		}
-		
-		arbutus.unsubscribe();
-		
-		System.exit(0);
-        
-		ServiceManager.getInstance().stopServices();
-		log.debug("Services stopped");
+		catch(Exception ex) {
+			log.error(ex);
+		}
+		finally {
+			arbutus.unsubscribe();
+
+			ServiceManager.getInstance().stopServices();
+			log.debug("Services stopped");
+			
+			System.exit(0);
+		}
 	}
 }
